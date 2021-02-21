9896bb5403474f375070af819e2d64056d8dcfb0
GabrieleFirriolo <gabrifirr@gmail.com>
2021-02-21 18:36:36 +0100

    Inserimento Funzioni AForge - Identificazione Glifo

---
+++ WEB SOCKET/AForge_Functions/Funzioni.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Vision.GlyphRecognition;
+
+
+namespace Glyphs
+{
+
+    public class Funzioni
+    {
+        public static void AddGlyphData()
+        {
+            GlyphDatabase glyphDatabase = new GlyphDatabase(5);
+
+            //1
+            glyphDatabase.Add(new Glyph("Creeper", new byte[5, 5] {
+            { 0, 0, 0, 0, 0 },
+            { 0, 1, 1, 0, 0 },
+            { 0, 0, 1, 1, 0 },
+            { 0, 0, 1, 0, 0 },
+            { 0, 0, 0, 0, 0 } }));
+            // 2
+            glyphDatabase.Add(new Glyph("Fly", new byte[5, 5] {
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 0 },
+            { 0, 1, 1, 0, 0 },
+            { 0, 1, 0, 1, 0 },
+            { 0, 0, 0, 0, 0 } }));
+            // 3
+            glyphDatabase.Add(new Glyph("Bug", new byte[5, 5] {
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0 },
+            { 0, 1, 1, 0, 0 },
+            { 0, 0, 1, 1, 0 },
+            { 0, 0, 0, 0, 0 } }));
+        }
+        public static int FindG(string img)
+        {
+            GlyphRecognizer recognizer = new GlyphRecognizer(5);
+            int x = 0;
+            Bitmap image;
+            try
+            {
+                image = new Bitmap($@"C:\Users\Gabry\Desktop\images\{img}.jpg", true);
+            }
+            catch
+            {
+                return -1;
+            }
+            List<ExtractedGlyphData> glyphs = recognizer.FindGlyphs(image);
+            foreach (ExtractedGlyphData glyphData in glyphs)
+            {
+                x += 1;
+            }
+            return x;
+        }
+    }
+}
