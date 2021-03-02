90edee9796d1ea2276f5076e65de4f311620b094
LORENZO SIGNORELLI <signolory@gmail.com>
2021-03-02 14:42:09 +0100

    serve id

---
+++ SERVER_IDGLYPHS/IDGLYPHS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Fleck;
+using Moq;
+namespace SERVER_IDGLYPHS
+{
+    interface IAforge
+    {
+        public string Getid();
+    }
+    class IDGLYPHS
+    {
+        static void Main(string[] args)
+        {
+            var Aforge = new Mock<IAforge>();
+            
+            var websocketServer = new WebSocketServer("ws://127.0.0.1:8181");
+            websocketServer.Start(connection =>
+            {
+
+                connection.OnOpen = () =>
+                {
+                    Console.WriteLine("CLIENT CONNESSO");
+                    connection.Send($"CONNESSO");
+                };
+                connection.OnClose = () =>
+                {
+                    Console.WriteLine("CLIENT DISCONNESSO");
+                };
+                connection.OnBinary = bytes =>
+                {
+                    Aforge.Setup(x => x.Getid()).Returns("ABC");
+                    connection.Send(Aforge.Object.Getid());
+                };
+
+                connection.OnError = exception =>
+                {
+                    Console.WriteLine($"OnError {exception.Message}");
+                };
+
+            });
+            string f = default;
+            while (f != "quit")
+            {
+                f = Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ SERVER_PERCORSO/PERCORSO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Fleck;
+
+namespace SERVER_PERCORSO
+{
+    class PERCORSO
+    {
+        static void Main(string[] args)
+        {
+
+
+            var websocketServer = new WebSocketServer("ws://127.0.0.1:8181");
+            websocketServer.Start(connection =>
+            {
+
+                connection.OnOpen = () =>
+                {
+                    Console.WriteLine("CLIENT CONNESSO");
+                    connection.Send($"CONNESSO");
+                };
+                connection.OnClose = () =>
+                {
+                    Console.WriteLine("CLIENT DISCONNESSO");
+                };
+                connection.OnBinary = bytes =>
+                {
+
+                };
+
+                connection.OnError = exception =>
+                {
+                    Console.WriteLine($"OnError {exception.Message}");
+                };
+
+            });
+            string f = default;
+            while (f != "quit")
+            {
+                f = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
