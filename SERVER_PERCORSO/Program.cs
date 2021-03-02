9b354cfbcd636c2e3704399a40fefd640687a2c8
LORENZO SIGNORELLI <signolory@gmail.com>
2021-03-02 11:54:03 +0100

    inizio creazone server multipli

---
+++ SERVER_PERCORSO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SERVER_PERCORSO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
