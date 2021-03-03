35bcb02f8b4982637913efa4002e9d61ccccd9ea
GhilardiAndrea <62284496+GhilardiAndrea@users.noreply.github.com>
2021-03-03 13:03:22 +0100

    Modifiche server ImmagineMappa

--- SERVER_IMMAGINEMAPPA/Program.cs
+++ SERVER_IMMAGINEMAPPA/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using Fleck;
 
 namespace SERVER_IMMAGINEMAPPA
 {
+    public class TabellaImmagineMappa
+    {
+        private string _codice;
+        public string codice
+        {
+            get
+            {
+                return _codice;
+            }
+        }
+
+        private decimal _x;
+        public decimal x
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                if(value<-90||value>90)
+                {
+                    throw new Exception("Coordinata x non valida");
+                }
+            }
+        }
+        private decimal _y;
+        public decimal y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                if (value < -180 || value > 180)
+                {
+                    throw new Exception("Coordinata y non valida");
+                }
+            }
+        }
+    }
     class ImmagineMappa
     {
+        static List<TabellaImmagineMappa> lista = new List<TabellaImmagineMappa>();
+        public static string CreaImmagine(string messaggio)
+        {  
+            var bus=lista.FirstOrDefault(l => l.codice == messaggio);
+            string immagine = default;
+            decimal x = bus.x;
+            decimal y = bus.y;
+            //Elaborazione
+            return immagine;
+        }
         static void Main(string[] args)
         {
+            List<TabellaImmagineMappa> lista = new List<TabellaImmagineMappa>();
+            object a = new object();
             var websocketServer = new WebSocketServer("ws://127.0.0.1:8181");
             Console.WriteLine("Server Immagine-Mappa");
             websocketServer.Start(connection =>
@@ -24,9 +81,8 @@ namespace SERVER_IMMAGINEMAPPA
                 };
                 connection.OnMessage = message =>
                 {
-                    if (message == "True")
-                        connection.Send("Immagine mappa");
-
+                    var immagine=CreaImmagine(message);
+                    connection.Send(immagine);
                 };
 
                 connection.OnError = exception =>
