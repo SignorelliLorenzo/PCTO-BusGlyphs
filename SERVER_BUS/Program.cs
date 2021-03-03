f9dd55d4711773eb4afaa9964edf0cd1fbe76090
GhilardiAndrea <62284496+GhilardiAndrea@users.noreply.github.com>
2021-03-03 12:08:34 +0100

    Implementazione server 3 e 4

--- SERVER_BUS/Program.cs
+++ SERVER_BUS/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using Fleck;
 
 namespace SERVER_BUS
 {
-    class Program
+    class Bus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var websocketServer = new WebSocketServer("ws://127.0.0.1:8181");
+            Console.WriteLine("Server Bus");
+            websocketServer.Start(connection =>
+            {
+
+                connection.OnOpen = () =>
+                {
+                    Console.WriteLine("CLIENT CONNESSO");
+                    connection.Send($"CONNESSO");
+                };
+                connection.OnClose = () =>
+                {
+                    Console.WriteLine("CLIENT DISCONNESSO");
+                };
+                connection.OnMessage = message =>
+                {
+                    if (message == "True")
+                        connection.Send("Bus Scelto");
+
+                };
+
+                connection.OnError = exception =>
+                {
+                    Console.WriteLine($"OnError {exception.Message}");
+                };
+
+            });
+            string fine = default;
+            while (fine != "quit")
+            {
+                
+                Console.WriteLine("Scrivere quit per uscire");
+                fine = Console.ReadLine();
+                fine = fine.Trim().ToLower();
+
+            }
         }
     }
 }
